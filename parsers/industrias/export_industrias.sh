6e426b5d1fd82db9bfe73a0c02c072361e643dbe
Christian Parsons <chparsons@gmail.com>
2013-09-11 14:40:15 -0300

    parser industrias

--- parsers/industrias/export_industrias.sh
+++ parsers/industrias/export_industrias.sh
@@ -3,9 +3,6 @@
 #./export_industrias.sh
 #exporta a industrias.csv por default
 
-#./export_industrias.sh > miarchivo.csv
-#exporta a un csv con nombre miarchivo
-
 python parser.py
 sqlite3 qpr.sqlite <<!
 .headers on
